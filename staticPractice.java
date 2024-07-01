8648d5a0a9aec819bb991a95590be700c0320c80
Vraj Patel <31vrajpatel@gmail.com>
2024-07-01 18:16:37 +0530

    object classes

---
+++ staticPractice.java
@@ -0,0 +1,61 @@
+class Mobile {
+
+      String brand;
+      int price;
+      static String name; // All the objects are refer the same static variable
+      // static variables are share by all the objects
+
+      // static block
+      static {
+            name = "Phone";
+            System.out.println("in static block");
+      }
+
+      // Constructor Class
+      public Mobile() {
+
+            brand = "";
+            System.out.println("in constructor block");
+      }
+
+      // simple show method
+      public void show() {
+
+            System.out.println(brand + " : " + price + " : " + name);
+      }
+
+      // static method
+      public static void show1() {
+            System.out.println("In a static method");
+      }
+
+}
+
+public class staticPractice {
+
+      public static void main(String[] args) throws ClassNotFoundException {
+
+            // if you want to load a class without any objects created...
+            // Class.forName("Mobile");
+
+            Mobile obj1 = new Mobile();
+            obj1.brand = "Samsung";
+            obj1.price = 1500;
+            Mobile.name = "Smartphone";
+
+            Mobile obj2 = new Mobile();
+            obj2.brand = "Apple";
+            obj2.price = 1299;
+            Mobile.name = "Smartphone";
+
+            Mobile.name = "Phone";
+
+            System.out.println();
+
+            obj1.show();
+            obj2.show();
+
+            Mobile.show1();
+
+      }
+}
---
+++ string.java
@@ -0,0 +1,43 @@
+public class string {
+
+      public static void main(String[] args) {
+
+            // String name = "naveen";
+
+            // name = name + " reddy";
+
+            // System.out.println("Hello " + name);
+
+            // // System.out.println(name.charAt(2));
+            // // System.out.println(name.concat(" reddy"));
+
+            // String s1 = "Naveen";
+            // String s2 = "Naveen";
+
+            // System.out.println(s1 == s2);
+            // System.out.println(s1);
+
+            // STRING BUFFER
+            // String Buffer is Threat Safe
+
+            // StringBuffer sb = new StringBuffer("Navin");
+            // System.out.println(sb.capacity());
+            // System.out.println(sb.length());
+
+            // sb.append("Reddy");
+
+            // System.out.println(sb);
+            // sb.deleteCharAt(3);
+            // System.out.println(sb);
+            // sb.insert(3, "ee");
+            // System.out.println(sb);
+
+            // System.out.println(sb.substring(2, 8));
+
+            // String str = sb.toString();
+
+
+            // STRING BUILDER
+            // String Builder is not Threat Safe. 
+      }
+}
